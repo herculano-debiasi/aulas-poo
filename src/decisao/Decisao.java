a0bcc6efc4e265a30fd2bf0dc49c09b782475420
Herculano De Biasi <02105018980@udesc.br>
2026-03-22 13:28:51 -0300

    Alteração de teste

--- src/decisao/Decisao.java
+++ src/decisao/Decisao.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Decisao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+	int numero;
 
         System.out.print("Entre com um número entre 1 e 50: ");
-        int numero = scanner.nextInt();
+        numero = scanner.nextInt();
 
         if (numero == 42) {
             System.out.println("Acertou!!!");
