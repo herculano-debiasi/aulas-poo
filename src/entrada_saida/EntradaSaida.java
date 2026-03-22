3c1e911a650b256e70c063c9c9756e1654ba942b
Herculano <02105018980@udesc.br>
2026-03-22 13:24:01 -0400

    Alteração de teste

--- src/entrada_saida/EntradaSaida.java
+++ src/entrada_saida/EntradaSaida.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class EntradaSaida {
     public static void main(String[] args) {
-        int idade;
         double salario;
 
         Scanner scanner = new Scanner(System.in);
@@ -13,7 +12,7 @@ public class EntradaSaida {
         String nome = scanner.nextLine();
 
         System.out.print("Digite a sua idade: ");
-        idade = scanner.nextInt();
+        int idade = scanner.nextInt();
 
         System.out.print("Digite o seu salário: ");
         salario = scanner.nextDouble();
